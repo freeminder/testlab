2a08296b57c2805f61186216890f72550b3cd66b
Dmitry Zhukov <dmitry.zhukov@gmail.com>
2018-10-05 05:13:20 +0300

    dependencies updated

--- foreign-exchange-rate-sinatra-react/src/components/currencies.jsx
+++ foreign-exchange-rate-sinatra-react/src/components/currencies.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 
 export default class Currencies extends Component {
   render() {
-    console.log(this.props);
     return (
       <div>
         <label>
